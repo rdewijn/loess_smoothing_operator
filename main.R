79c40741f01fbb309c8aff60585fea19155aa3e0
Rik <rdewijn@xs4all.nl>
2021-04-18 14:57:55 +0000

    added regress mode
    added optional labels for mapping back to variables

--- main.R
+++ main.R
@@ -1,7 +1,16 @@
 library(tercen)
 library(dplyr)
 
-do.loess <- function(df, n_preds, span) {
+do.loess <- function(df, n_preds, span, mode) {
+  if (mode == "smooth"){
+    result = loess.smooth(df, n_preds, span)
+  } else {
+    result = loess.regress(df, span)
+  }
+  return(result)
+}
+
+loess.smooth = function(df, n_preds, span){
   m <- loess(.y ~ .x, data = df, span = span)
   x_pred <- seq(min(df$.x), max(df$.x), length.out = n_preds)
   y_pred <- predict(m, newdata = data.frame(.x = x_pred))
@@ -11,20 +20,58 @@ do.loess <- function(df, n_preds, span) {
     x_pred = x_pred,
     y_pred = y_pred
   )
-  return(df_out)
 }
 
-(ctx = tercenCtx())
+loess.regress = function(df, span){
+  m = loess(.y ~ .x, data = df, span = span)
+  df_out = data.frame(
+    .ri = df$.ri[1],
+    .ci = df$.ci[1],
+    .x = df$.x,
+    .y = df$.y,
+    yFit = predict(m),
+    yRes = resid(m)
+  )
+  if (!is.null(df$labels))
+    df_out = data.frame(df_out, labels = df$labels)
+  
+  return(df_out)
+  
+}
+
+
+
+ctx = tercenCtx()
 
 n_preds <- 1000
 if(!is.null(ctx$op.value('n_preds'))) n_preds <- as.numeric(ctx$op.value('n_preds'))
 span <- 0.75
 if(!is.null(ctx$op.value('span'))) span <- as.numeric(ctx$op.value('span'))
+operatorMode = "regress"
+if(!is.null(ctx$op.value('mode'))) mode <- as.numeric(ctx$op.value('mode'))
 
-ctx %>% 
-  select(.ci, .ri, .x, .y) %>%
+df = ctx %>% 
+  select(.ci, .ri, .x, .y)
+
+if(length(ctx$labels) ==1){
+  df = df %>% 
+    bind_cols(ctx$select(ctx$labels)) %>%
+    select(.ci, .ri, .x, .y, labels = all_of(ctx$labels[[1]]))
+}
+
+result = df %>%
   group_by(.ci, .ri) %>%
-  do(do.loess(., n_preds, span)) %>% 
+  do(do.loess(., n_preds, span, operatorMode))
+
+if(!is.null(result$labels)){
+  mapname = paste0(ctx$labels[[1]],".labels")
+  result = result %>% rename(!!mapname:=labels)
+}
+  
+
+result %>%
   ctx$addNamespace() %>%
   ctx$save()
+
+
 
--- workspace.R
+++ workspace.R
@@ -1,33 +1,7 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "d330322c43363eb4f9b27738ef0042b9")
-options("tercen.stepId"     = "1ae42627-e9ce-4d9f-9797-8700adfd7718")
+options("tercen.workflowId" = "43f6de57a8befbf60bbcb444f30062e0")
+options("tercen.stepId"     = "c9136dba-57c0-4305-9db2-c4da31aa7bc5")
 
-do.loess <- function(df, n_preds, span) {
-  m <- loess(.y ~ .x, data = df, span = span)
-  x_pred <- seq(min(df$.x), max(df$.x), length.out = n_preds)
-  y_pred <- predict(m, newdata = data.frame(.x = x_pred))
-  df_out <- data.frame(
-    .ri = df$.ri[1],
-    .ci = df$.ci[1],
-    x_pred = x_pred,
-    y_pred = y_pred
-  )
-  return(df_out)
-}
-
-(ctx = tercenCtx())
-
-n_preds <- 1000
-if(!is.null(ctx$op.value('n_preds'))) n_preds <- as.numeric(ctx$op.value('n_preds'))
-span <- 0.75
-if(!is.null(ctx$op.value('span'))) span <- as.numeric(ctx$op.value('span'))
-
-ctx %>% 
-  select(.ci, .ri, .x, .y) %>%
-  group_by(.ci, .ri) %>%
-  do(do.loess(., n_preds, span)) %>% 
-  ctx$addNamespace() %>%
-  ctx$save()
-
+source("./main.R")
